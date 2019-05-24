1a84350a63542b2defd8dd01c6f6a2adaeb9de56
aev-mambro2 <42442483+aev-mambro2@users.noreply.github.com>
2019-05-24 13:53:16 -0400

    Used a map to load data into the account struct.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,35 @@
 mod com;
 extern crate secstr;
-use secstr::*;
 use crate::com::mambro::domain::{
     Account, AccountId, ConfigId, Credentials, FileLocation, FileLocationIntentions, FileName,
     FolderName, IMaybeEmpty, Key, Secret, ThirdPartyId, Token, URI,
 };
+use secstr::*;
 use std::collections::HashMap;
 use std::error::Error;
 use std::result;
 
-fn build(map: HashMap<String, String>) -> result::Result<Account, Box<Error>> {
+fn build(data: HashMap<&str, &str>) -> result::Result<Account, Box<Error>> {
     let account = Account {
         config_id: ConfigId {
-            account_id: AccountId(String::from("a")),
-            third_party_id: ThirdPartyId(String::from("b")),
+            account_id: AccountId(data.get("id").unwrap_or(&"").to_string()),
+            third_party_id: ThirdPartyId(data.get("third_party_id").unwrap_or(&"").to_string()),
         },
         credentials: Credentials {
-            uri: URI(String::from("https://here/")),
+            uri: URI(data.get("uri").unwrap_or(&"").to_string()),
             app: Token {
-                key: Key(SecStr::from("a")),
-                secret: Secret(SecStr::from("b")),
+                key: Key(SecStr::from(data.get("app_key").unwrap_or(&"").to_string())),
+                secret: Secret(SecStr::from(data.get("app_secret").unwrap_or(&"").to_string())),
             },
             user: Token {
-                key: Key(SecStr::from("a")),
-                secret: Secret(SecStr::from("b")),
+                key: Key(SecStr::from(data.get("user_key").unwrap_or(&"").to_string())),
+                secret: Secret(SecStr::from(data.get("user_token").unwrap_or(&"").to_string())),
             },
         },
         location: FileLocation {
             intent: FileLocationIntentions::ReadFrom,
-            folder: FolderName(String::from("1")),
-            name: FileName(String::from("2")),
+            folder: FolderName(data.get("readfrom_folder").unwrap_or(&"").to_string()),
+            name: FileName(data.get("readfrom_name").unwrap_or(&"").to_string()),
         },
     };
     if account.is_empty() {
@@ -39,7 +39,18 @@ fn build(map: HashMap<String, String>) -> result::Result<Account, Box<Error>> {
 }
 
 fn main() {
-    let maybe_account = build(HashMap::new());
+    let mut account_data = HashMap::new();
+    account_data.insert("id", "a");
+    account_data.insert("third_party_id", "b");
+    account_data.insert("uri", "https://here/");
+    account_data.insert("app_key", "a");
+    account_data.insert("app_secret", "b");
+    account_data.insert("user_key", "c");
+    account_data.insert("user_token", "d");
+    account_data.insert("readfrom_folder", "1");
+    account_data.insert("readfrom_name", "2");
+
+    let maybe_account = build(account_data);
     match maybe_account {
         Ok(account) => {
             panic_if_empty(&account);
