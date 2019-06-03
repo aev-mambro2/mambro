a0b8c7da32e1454acb400578eb54874c6ccff966
aev-mambro2 <42442483+aev-mambro2@users.noreply.github.com>
2019-06-03 12:09:24 +0000

    Added models mirroring new db structures.

--- src/com/mambro/db/models.rs
+++ src/com/mambro/db/models.rs
@@ -14,3 +14,62 @@ pub struct Accounts {
     pub modified_at: String,
     pub version: i32,
 }
+
+#[derive(Queryable)]
+pub struct EmailAddresses {
+    pub id: String,
+    pub inserted_at: String,
+    pub modified_at: String,
+    pub version: i32,
+}
+
+#[derive(Queryable)]
+pub struct EmailReports {
+    pub id: String,
+    pub inserted_at: String,
+    pub modified_at: String,
+    pub version: i32,
+}
+
+#[derive(Queryable)]
+pub struct Applications {
+    pub id: String,
+    pub inserted_at: String,
+    pub modified_at: String,
+    pub version: i32,
+}
+
+#[derive(Queryable)]
+pub struct FileLocationPurposes {
+    pub id: String,
+    pub inserted_at: String,
+    pub modified_at: String,
+    pub version: i32,
+}
+
+#[derive(Queryable)]
+pub struct FileLocations {
+    pub purpose: String,
+    pub account: String,
+    pub third_party: String,
+    pub for_reading: i32,
+    pub for_writing: i32,
+    pub folder: String,
+    pub name: String,
+    pub inserted_at: String,
+    pub modified_at: String,
+    pub version: i32,
+}
+
+#[derive(Queryable)]
+pub struct EmailReportSubscriptions {
+    pub report: String,
+    pub account: String,
+    pub third_party: String,
+    pub application: String,
+    pub address: String,
+    pub name: String,
+    pub inserted_at: String,
+    pub modified_at: String,
+    pub version: i32,
+}
