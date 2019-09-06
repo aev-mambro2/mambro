f525df22053619cebe146709ee7973099aaff2ea
aev-mambro2 <42442483+aev-mambro2@users.noreply.github.com>
2019-09-06 22:48:02 +0000

    lib apiconsumer now has working xpath extractor.

--- lib/apiconsumer/src/lib.rs
+++ lib/apiconsumer/src/lib.rs
@@ -1,34 +1,40 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn testParse() {
+    fn test_parse() {
         extern crate sxd_document;
         extern crate sxd_xpath;
 
+        use super::xpath as xpath;
         use sxd_document::parser;
-        use sxd_xpath::{evaluate_xpath, Value};
+        use sxd_xpath::evaluate_xpath;
 
-        let package = parser::parse("<root>hello</root>").expect("failed to parse XML");
+        let input = "<root>hello</root>";
+        let package = parser::parse(input).expect("failed to parse XML");
         let document = package.as_document();
 
-        let value = evaluate_xpath(&document, "/root").expect("XPath evaluation failed");
+        let statement: &str = "/root";
+        let value = evaluate_xpath(&document, statement).expect("XPath evaluation failed");
 
         assert_eq!("hello", value.string());
+
+        let extracted: String = xpath::extract(input, statement);
+
+        assert_eq!("hello", extracted.as_str());
     }
 }
-mod xml {
+mod xpath {
     extern crate sxd_document;
     extern crate sxd_xpath;
 
     use sxd_document::parser;
-    use sxd_xpath::{evaluate_xpath, Value};
+    use sxd_xpath::evaluate_xpath;
 
-    fn parse() {
-        let package = parser::parse("<root>hello</root>").expect("failed to parse XML");
+    pub fn extract(d: &str, s: &str) -> String {
+        let package = parser::parse(d).expect("failed to parse XML");
         let document = package.as_document();
-
-        let value = evaluate_xpath(&document, "/root").expect("XPath evaluation failed");
-
-        assert_eq!("hello", value.string());
+        let extracted = evaluate_xpath(&document, s).expect("XPath evaluation failed");
+        let output = extracted.string();
+        output
     }
 }
