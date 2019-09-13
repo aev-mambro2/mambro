a5a234c6c820c12586c0adc595e91b4df08e2c98
aev-mambro2 <42442483+aev-mambro2@users.noreply.github.com>
2019-09-13 20:56:58 +0000

    Improving documentation

--- lib/apiconsumer/src/lib.rs
+++ lib/apiconsumer/src/lib.rs
@@ -23,6 +23,10 @@ mod tests {
         assert_eq!("hello", extracted.as_str());
     }
 }
+
+// Contains convenience methods for extracting
+// information from XML documents using XPath
+// statements.
 mod xpath {
     extern crate sxd_document;
     extern crate sxd_xpath;
@@ -30,6 +34,30 @@ mod xpath {
     use sxd_document::parser;
     use sxd_xpath::evaluate_xpath;
 
+    // Extracts the passed-in XPath statement
+    // from the passed-in document as a String.
+    // Use this if you have a single XPath
+    // statement to resolve. If you have 
+    // several, use extract(&str, Vec<&str>)
+    // instead. 
+    //
+    // # Parameters
+    // - d: a referenced string slice containing 
+    //   an XML-formated document. 
+    // - s: an XPath statement.
+    //   
+    // # Returns
+    // Either an empty string, or the value 
+    // found by evaluating the XPath statement
+    // against the document.
+    //
+    /// # Example
+    ///    use crate::xpath as xpath;
+    ///    let input = "<root>hello</root>";
+    ///    let statement: &str = "/root";
+    ///    let extracted: String = xpath::extract(input, statement);
+    ///    assert_eq!("hello", extracted.as_str());
+    ///
     pub fn extract(d: &str, s: &str) -> String {
         let package = parser::parse(d).expect("failed to parse XML");
         let document = package.as_document();
--- lib/domain/src/lib.rs
+++ lib/domain/src/lib.rs
@@ -1,65 +1,65 @@
-// Domain contains data types that are
-// used in data integration applications
-// that connect local ERPs to remote
-// channels, a.k.a. third parties.
-//
-// Those often require account inform-
-// ation and credentials for authoriz-
-// ing communications.
-//
-// Each account tends to read the info
-// it needs to send from file locations
-// that are marked for a particular
-// communication purpose.
-//
-// When communications have completed,
-// users and managers expect some notific-
-// ation. Thus, each account owns a list
-// of email addresses, each of which is
-// marked for a purpose, and sometimes
-// exclusive to an application.
-//
-// Author: A.E.Veltstra
-// Since: 2.19.501.900
-// Version: 2.19.906.2131
+//! Domain contains data types that are used 
+//! in data integration applications that 
+//! connect local ERPs to remote channels, 
+//! a.k.a. third parties.
+//!
+//! Those often require account information 
+//! and credentials for authorizing 
+//! communications.
+//!
+//! Each account tends to read the info it 
+//! needs to send from file locations that 
+//! are marked for a particular communication 
+//! purpose.
+//!
+//! When communications have completed, users 
+//! and managers expect some notification. 
+//! Thus, each account owns a list of email 
+//! addresses, each of which is marked for a 
+//! purpose, and sometimes exclusive to an 
+//! application.  
+//!
+//! Author: A.E.Veltstra  
+//! Since: 2.19.501.900  
+//! Version: 2.19.906.2131  
 
 // For compiling and debugging diesel,
 // we are required to increase the
 // recursion limit.
 #![recursion_limit = "128"]
 
-// Prelude contains things that should have
-// been included in the main diesel crate,
-// but whatever.
+/// Prelude contains things that should have
+/// been included in the main diesel crate,
+/// but whatever.
 extern crate diesel;
 use diesel::prelude::*;
 
-// SecStr destroys sensitive data from
-// memory once done.
+/// SecStr destroys sensitive data from
+/// memory once done.
 extern crate secstr;
 use secstr::*;
 
-// Mambro db knows the layout of the
-// data store.
+/// Mambro db knows the layout of the
+/// data store.
 extern crate db;
 
-// Db models contain rust-based data
-// wrappers around the data store's
-// structures.
+/// Db models contain rust-based data
+/// wrappers around the data store's
+/// structures.
 extern crate models;
 
-// OsStr is used for O.S.-specific texts
-// (like file paths and names), that can
-// contain characters which don't fit into
-// utf-8.
+/// OsStr is used for O.S.-specific texts
+/// (like file paths and names), that can
+/// contain characters which don't fit into
+/// utf-8.
 use std::ffi::OsStr;
 
-// Fmt helps us share human-readable views
-// on the data retained by the data types
-// in this module.
+/// Fmt helps us share human-readable views
+/// on the data retained by the data types
+/// in this module.
 use std::fmt;
 
-// Path and PathBuf handle file locations.
+/// Path and PathBuf handle file locations.
 use std::path::{Path, PathBuf};
 
 /// Identifies an account. Gets combined
@@ -72,87 +72,150 @@ use std::path::{Path, PathBuf};
 ///
 /// # Example
 ///
+/// ```rust
+/// use crate::domain::AccountId;  
+/// let id = AccountId::from("Hot Topic");  
+/// assert_eq!("Hot Topic".to_string(), id.to_string());  
 /// ```
-/// use crate::domain::AccountId;
-/// let id = AccountId::from("Hot Topic");
-/// assert_eq!("Hot Topic".to_string(), id.to_string());
-/// ```
+///
 #[derive(Clone, Debug, PartialEq)]
 pub struct AccountId(Vec<u8>);
+
+/// Convert from a common string slice.
 impl From<&str> for AccountId {
-    // # Arguments
-    // - &str: a name that identifies an
-    //         account. Should match a name
-    //         in the data store. The name
-    //         gets copied and owned by the
-    //         instance.
-    //
+    /// # Arguments
+    ///
+    /// - &str: a name that identifies an
+    ///         account. Should match a name
+    ///         in the data store. The name
+    ///         gets copied and owned by the
+    ///         instance.
+    ///
     fn from(s: &str) -> Self {
         AccountId::from(s)
     }
 }
+
+/// Converts from a common string.
 impl From<String> for AccountId {
+    /// # Arguments
+    ///
+    /// - String: a name that identifies an
+    ///         account. Should match a name
+    ///         in the data store. The name
+    ///         gets copied and owned by the
+    ///         instance.
+    ///
     fn from(s: String) -> Self {
         AccountId::from(s.as_str())
     }
 }
+
+
 impl AccountId {
-    // # Arguments
-    // - &str: a name that identifies an
-    //         account. Should match a name
-    //         in the data store. The name
-    //         gets copied and owned by the
-    //         instance.
-    //
+    /// Converts from a common string slice.
+    ///
+    ///
+    /// # Arguments
+    ///
+    /// - &str: a name that identifies an
+    ///         account. Should match a name
+    ///         in the data store. The name
+    ///         gets copied and owned by the
+    ///         instance.
+    ///
     fn from(s: &str) -> AccountId {
         AccountId(s.as_bytes().to_vec())
     }
 
-    // Returns an owned, immutable copy of
-    // the name retained in this instance.
+    /// Returns an owned, immutable copy of
+    /// the name retained in this instance.
     pub fn to_string(&self) -> String {
         String::from_utf8(self.0.as_slice().to_vec()).unwrap()
     }
 }
 
-// The intended use of a file location.
-// Gets combined with a folder path
-// and file name in a FileLocation.
+/// The intended use of a file location.
+/// Gets combined with a folder path
+/// and file name in a FileLocation.
 ///
 ///
 /// # Example
 ///
+/// ```rust
+/// use crate::domain::FileLocationPurpose;  
+/// let p = FileLocationPurpose::from("orders");  
+/// assert_eq!("orders".to_string(), p.to_string());  
 /// ```
-/// use crate::domain::FileLocationPurpose;
-/// let p = FileLocationPurpose::from("orders");
-/// assert_eq!("orders".to_string(), p.to_string());
-/// ```
+///
 #[derive(Clone, Debug, PartialEq)]
 pub struct FileLocationPurpose(Vec<u8>);
+
+// Documenting this is useless: 
+// it gets documented using standard text
+// yanked from elsewhere. 
 impl PartialEq<&str> for FileLocationPurpose {
     fn eq(&self, s: &&str) -> bool {
         let other = FileLocationPurpose::from(*s);
         self.eq(&other)
     }
 }
-// To make the purpose display as a word,
-// rather than a byte array. Failed.
+/// To make the purpose display as a word,
+/// rather than a byte array. 
+// Failed.
 impl fmt::Display for FileLocationPurpose {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         fmt::Display::fmt(&self.to_string(), f)
     }
 }
+/// Convert from a common string slice.
 impl From<&str> for FileLocationPurpose {
+    /// Converts from a common string slice.
+    ///
+    ///
+    /// # Arguments
+    ///
+    /// - &str: a name that identifies why a
+    ///         location is used. Should 
+    ///         match a purpose in the data 
+    ///         store. The name gets copied 
+    ///         and owned by the instance.
+    ///
     fn from(s: &str) -> Self {
         FileLocationPurpose::from(s)
     }
 }
+
+/// Convert from a common string.
 impl From<String> for FileLocationPurpose {
+    /// Converts from a common string.
+    ///
+    ///
+    /// # Arguments
+    ///
+    /// - String: a name that identifies why a
+    ///         location is used. Should 
+    ///         match a purpose in the data 
+    ///         store. The name gets copied 
+    ///         and owned by the instance.
+    ///
     fn from(s: String) -> Self {
         FileLocationPurpose::from(s.as_str())
     }
 }
+
 impl FileLocationPurpose {
+    /// Converts from a common string slice.
+    ///
+    ///
+    /// # Arguments
+    ///
+    /// - &str: a name that identifies why a
+    ///         location is used. Should 
+    ///         match a purpose in the data 
+    ///         store. The name gets copied 
+    ///         and owned by the instance.
+    ///
     fn from(s: &str) -> FileLocationPurpose {
         FileLocationPurpose(s.as_bytes().to_vec())
     }
@@ -161,34 +224,73 @@ impl FileLocationPurpose {
     }
 }
 
-// Identifies a remote party. Gets combined
-// with an AccountId in a ConfigId, which
-// must be unique.
-//
-// Assets like file locations and email
-// addresses get assigned to ConfigIds.
+/// Identifies a remote party. Gets combined
+/// with an AccountId in a ConfigId, which
+/// must be unique.
+///
+/// Assets like file locations and email
+/// addresses get assigned to ConfigIds.
 ///
 ///
 /// # Example
 ///
+/// ```rust
+/// use crate::domain::ThirdPartyId;  
+/// let id = ThirdPartyId::from("Spencers");  
+/// assert_eq!("Spencers".to_string(), id.to_string());  
 /// ```
-/// use crate::domain::ThirdPartyId;
-/// let id = ThirdPartyId::from("Spencers");
-/// assert_eq!("Spencers".to_string(), id.to_string());
-/// ```
+///
 #[derive(Clone, Debug, PartialEq)]
 pub struct ThirdPartyId(Vec<u8>);
+
+/// Convert from a common string slice.
 impl From<&str> for ThirdPartyId {
+    /// Converts from a common string slice.
+    ///
+    ///
+    /// # Arguments
+    ///
+    /// - &str: a name that identifies a
+    ///         remote partner. Should match
+    ///         a third party in the data 
+    ///         store. The name gets copied 
+    ///         and owned by the instance.
+    ///
     fn from(s: &str) -> Self {
         ThirdPartyId::from(s)
     }
 }
+
+/// Convert from a common string.
 impl From<String> for ThirdPartyId {
+    /// Converts from a common string.
+    ///
+    ///
+    /// # Arguments
+    ///
+    /// - String: a name that identifies a
+    ///         remote partner. Should match
+    ///         a third party in the data 
+    ///         store. The name gets copied 
+    ///         and owned by the instance.
+    ///
     fn from(s: String) -> Self {
         ThirdPartyId::from(s.as_str())
     }
 }
+
 impl ThirdPartyId {
+    /// Converts from a common string slice.
+    ///
+    ///
+    /// # Arguments
+    ///
+    /// - &str: a name that identifies a
+    ///         remote partner. Should match
+    ///         a third party in the data 
+    ///         store. The name gets copied 
+    ///         and owned by the instance.
+    ///
     fn from(s: &str) -> ThirdPartyId {
         ThirdPartyId(s.as_bytes().to_vec())
     }
@@ -197,35 +299,89 @@ impl ThirdPartyId {
     }
 }
 
-// The address via which communication
-// should happen for an account at a
-// third party. Gets combined with
-// authentication tokens in Credentials.
-//
-// This may be replaced with a standard
-// data type.
+/// The address via which communication
+/// should happen for an account at a
+/// third party. Gets combined with
+/// authentication tokens in Credentials.
+///
+/// This may be replaced with a standard
+/// data type.
 ///
 ///
 /// # Example
 ///
+/// ```rust
+/// use crate::domain::URI;  
+/// let uri = URI::from("how://there.that/");  
+/// assert_eq!("how://there.that/".to_string(), uri.to_string());  
 /// ```
-/// use crate::domain::URI;
-/// let uri = URI::from("how://there.that/");
-/// assert_eq!("how://there.that/".to_string(), uri.to_string());
-/// ```
+///
 #[derive(Clone, Debug, PartialEq)]
 pub struct URI(Vec<u8>);
+
+/// Convert from a common string slice.
 impl From<&str> for URI {
+    /// Converts from a common string slice.
+    ///
+    ///
+    /// # Arguments
+    ///
+    /// - &str: a web address to use for 
+    ///         reaching out to the remote 
+    ///         partner's web / REST servers. 
+    ///         Suffix with / if the last 
+    ///         part is a TLD or a folder. 
+    ///         Omit the end / if the last
+    ///         part is a document or query 
+    ///         value.
+    ///         The address gets copied 
+    ///         and owned by the instance.
+    ///
     fn from(s: &str) -> Self {
         URI::from(s)
     }
 }
+
+/// Convert from a common string slice.
 impl From<String> for URI {
+    /// Converts from a common string..
+    ///
+    ///
+    /// # Arguments
+    ///
+    /// - &str: a web address to use for 
+    ///         reaching out to the remote 
+    ///         partner's web / REST servers. 
+    ///         Suffix with / if the last 
+    ///         part is a TLD or a folder. 
+    ///         Omit the end / if the last
+    ///         part is a document or query 
+    ///         value.
+    ///         The address gets copied 
+    ///         and owned by the instance.
+    ///
     fn from(s: String) -> Self {
         URI::from(s.as_str())
     }
 }
+
 impl URI {
+    /// Converts from a common string slice.
+    ///
+    ///
+    /// # Arguments
+    ///
+    /// - &str: a web address to use for 
+    ///         reaching out to the remote 
+    ///         partner's web / REST servers. 
+    ///         Suffix with / if the last 
+    ///         part is a TLD or a folder. 
+    ///         Omit the end / if the last
+    ///         part is a document or query 
+    ///         value.
+    ///         The address gets copied 
+    ///         and owned by the instance.
+    ///
     fn from(s: &str) -> URI {
         URI(s.as_bytes().to_vec())
     }
@@ -350,6 +506,12 @@ impl IMaybeEmpty for Token {
     }
 }
 impl Token {
+    /// Create from 2 common string slices.
+    /// Internally uses SecStr for holding 
+    /// each passed-in value. SecStr not 
+    /// only masks the value when displaying,
+    /// it also destroys the value from 
+    /// memory after use.
     pub fn new(k: &str, s: &str) -> Self {
         Token {
             key: SecUtf8::from(k.as_bytes()),
@@ -450,6 +612,10 @@ impl crate::FileLocation {
         buffer.dedup();
         buffer
     }
+
+    /// Create from 3 common string slices.
+    /// The new instance winds up owning the 
+    /// passed-in contents.
     pub fn new(p: &str, f: &str, n: &str) -> crate::FileLocation {
         FileLocation {
             purpose: FileLocationPurpose::from(p),
