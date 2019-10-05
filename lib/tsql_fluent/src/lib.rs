33ddf5c14dc02e3c9f65000892ee9d0db9f7b8b5
aev-mambro2 <42442483+aev-mambro2@users.noreply.github.com>
2019-10-05 13:26:19 +0000

    added method to wrap verbs in a prefix and suffix.

--- lib/tsql_fluent/src/lib.rs
+++ lib/tsql_fluent/src/lib.rs
@@ -15,6 +15,56 @@ pub struct SqlBuilder {
     buffer: String,
 }
 
+/// Wraps the input in prefix and suffix if it isn't already.
+///
+/// # Examples
+///
+/// ```rust
+/// use tsql_fluent::*;
+/// let s = "verb".to_string();
+/// let w = wrap(s, "f", "5");
+/// assert_eq!("fverb5".to_string(), w);
+/// ```
+pub fn wrap(input: String, prefix: &str, suffix: &str) -> String {
+    if input.is_empty() {
+        if "" == prefix && "" == suffix {
+            return input;
+        }
+        let b1 = String::new() + prefix + suffix;
+        return b1;
+    }
+    if "" == prefix && "" == suffix {
+        return input;
+    }
+    if input.starts_with(prefix) && input.ends_with(suffix) {
+        return input;
+    }
+    let b2 = prefix.to_owned() + &input.clone();
+    let b3 = b2 + suffix;
+    return b3;
+}
+pub trait WrapInBrackets {
+    /// Wraps the input in brackets if it isn't already.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use tsql_fluent::*;
+    /// let s = "verb".to_string();
+    /// let w = s.wrap_in_brackets();
+    /// assert_eq!("[verb]".to_string(), w);
+    /// ```
+    fn wrap_in_brackets(self) -> String;
+}
+
+const BRACKET_OPEN: &'static str = "[";
+const BRACKET_CLOSE: &'static str = "]";
+impl WrapInBrackets for String {
+    fn wrap_in_brackets(self) -> String {
+        wrap(self, BRACKET_OPEN, BRACKET_CLOSE)
+    }
+}
+
 const SELECT: &'static str = "select ";
 const TOP: &'static str = "top ";
 
@@ -22,7 +72,7 @@ const TOP: &'static str = "top ";
 ///
 /// # Examples
 /// ```rust
-/// use sql::*;
+/// use tsql_fluent::*;
 /// let s = select();
 /// assert_eq!(s.to_string(), "select ".to_string());
 /// ```
@@ -37,13 +87,13 @@ pub fn select() -> SqlBuilder {
 ///
 /// # Examples
 /// ```rust
-/// use sql::*;
+/// use tsql_fluent::*;
 /// let n = "id".to_string();
 /// let s = select_n(n);
-/// assert_eq!(s.to_string(), "select id ".to_string());
+/// assert_eq!(s.to_string(), "select [id] ".to_string());
 /// ```
 pub fn select_n(n: String) -> SqlBuilder {
-    let b = format!("{}{} ", SELECT, n);
+    let b = format!("{}{} ", SELECT, n.wrap_in_brackets().as_str());
     SqlBuilder { buffer: b }
 }
 
@@ -54,23 +104,27 @@ pub fn select_n(n: String) -> SqlBuilder {
 ///
 /// # Examples
 /// ```rust
-/// use sql::*;
+/// use tsql_fluent::*;
 /// let a = "a".to_string();
 /// let b = "b".to_string();
 /// let c = "c".to_string();
 /// let m = vec![a, b, c];
 /// let s = select_m(m);
-/// assert_eq!(s.to_string(), "select a, b, c ".to_string());
+/// assert_eq!(s.to_string(), "select [a], [b], [c] ".to_string());
 /// ```
 pub fn select_m(m: Vec<String>) -> SqlBuilder {
     let mut b = SELECT.to_owned();
     if let Some((last, more)) = m.split_last() {
         for f in more {
-            b.push_str(f);
-            b.push_str(", ");
+            let f1 = f.clone().wrap_in_brackets();
+            let f2 = f1.as_str();
+            b = b + f2;
+            b = b + ", ";
         }
-        b.push_str(last);
-        b.push_str(" ");
+        let f3 = last.clone().wrap_in_brackets();
+        let f4 = f3.as_str();
+        b = b + f4;
+        b = b + " ";
     }
 
     let c = b;
@@ -90,7 +144,7 @@ impl SqlBuilder {
     /// 
     /// # Examples
     /// ```rust
-    /// use sql::*;
+    /// use tsql_fluent::*;
     /// let t = select().top();
     /// assert_eq!(t.to_string(), "select top ".to_string());
     /// ```
@@ -106,7 +160,7 @@ impl SqlBuilder {
     /// 
     /// # Examples
     /// ```rust
-    /// use sql::*;
+    /// use tsql_fluent::*;
     /// let t = select().top_x(5);
     /// assert_eq!(t.to_string(), "select top 5 ".to_string());
     /// ```
