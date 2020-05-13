5d8027968088e2e24d55ac83a4900b815a4a9520
aev-mambro2 <42442483+aev-mambro2@users.noreply.github.com>
2020-05-13 10:03:31 -0400

    Resolved working of Domain: finding an account and its file paths.

--- lib/learning-haskell/One/src/lists.hs
+++ lib/learning-haskell/One/src/lists.hs
@@ -1,15 +1,15 @@
 {-
  - Excercises concerning lists and operations on lists
  - @author A.E.Veltstra
- - @since 2020-05-01T13:29:00.000EST
- - @version 2020-05-01T13:30:00.000EST
+ - @since 2020-05-01T13:29:00.000EDT
+ - @version 2020-05-12T14:36:00.000EDT
  -}
 
 import Prelude
 import Data.Char
 
 digitsOnly :: String -> String
-digitsOnly that = [ it | it <- that, isDigit it ]
+digitsOnly x = [ it | it <- x, isDigit it ]
 
 allCharsAreDigits :: String -> Bool
 allCharsAreDigits xs = (xs == [x | x <- xs, isDigit x])
@@ -18,23 +18,23 @@ doubleEach :: [Integer] -> [Integer]
 doubleEach xs = [ 2*x | x <- xs ]
 
 makeAllCaps :: String -> String
-makeAllCaps that = [ toUpper it | it <- that ]
+makeAllCaps x = [ toUpper y | y <- x ]
 
 lettersOnly :: String -> String
-lettersOnly that = [ it | it <- that, isLetter it ]
+lettersOnly x = [ y | y <- x, isLetter y ]
 
 makeAllCapsLettersOnly :: String -> String
-makeAllCapsLettersOnly that = makeAllCaps (lettersOnly that)
+makeAllCapsLettersOnly x = makeAllCaps (lettersOnly x)
 
 findDivisors :: Integer -> [Integer]
-findDivisors that = [ it | it <- [1..that], mod that it == 0 ]
+findDivisors x = [ y | y <- [1..x], mod x y == 0 ]
 
 isPrime :: Integer -> Bool
-isPrime that 
-  | 0 == that = True
-  | 1 == that = True
-  | otherwise = ([1,that] == findDivisors that)
+isPrime x 
+  | 0 == x = True
+  | 1 == x = True
+  | otherwise = ([1,x] == findDivisors x)
 
 findPrimes :: (Integer, Integer) -> [Integer]
-findPrimes (a, b) = [ x | x <- [a..b], (isPrime x) ]
+findPrimes (x, y) = [ z | z <- [x..y], (isPrime z) ]
 
