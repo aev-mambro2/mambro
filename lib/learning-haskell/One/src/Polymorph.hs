7b6d6a73ff91b946cdd945b59d954c497fb5ed17
aev-mambro2 <42442483+aev-mambro2@users.noreply.github.com>
2020-05-21 15:26:07 -0400

    Implementing function layout variations and unit tests.

---
+++ lib/learning-haskell/One/src/Polymorph.hs
@@ -0,0 +1,7 @@
+{-| Polymorphism
+ -  Author: A.E.Veltstra
+ -  Since: 20200520
+ -  Version: 20200520
+ -}
+
+
--- lib/learning-haskell/One/src/lists.hs
+++ lib/learning-haskell/One/src/lists.hs
@@ -2,7 +2,7 @@
  - Excercises concerning lists and operations on lists
  - @author A.E.Veltstra
  - @since 2020-05-01T13:29:00.000EDT
- - @version 2020-05-12T14:36:00.000EDT
+ - @version 2020-05-21T15:02:00.000EDT
  -}
 
 import Prelude
@@ -26,15 +26,42 @@ lettersOnly x = [ y | y <- x, isLetter y ]
 makeAllCapsLettersOnly :: String -> String
 makeAllCapsLettersOnly x = makeAllCaps (lettersOnly x)
 
+-- |Find all numbers that divide the input without a remainder.
 findDivisors :: Integer -> [Integer]
 findDivisors x = [ y | y <- [1..x], mod x y == 0 ]
 
+-- |Whether or not a number is a prime (i.e.: can be divided
+-- only by 1 and itself).
 isPrime :: Integer -> Bool
+{-
+--This implementation uses guards
 isPrime x 
   | 0 == x = True
   | 1 == x = True
   | otherwise = ([1,x] == findDivisors x)
+-}
+--This implementation uses pattern matching
+isPrime 0 = True
+isPrime 1 = True
+isPrime x = ([1,x] == findDivisors x)
+
+-- |Tests isPrime and reports the test result
+testIsPrime :: IO ()
+testIsPrime = do
+  let a = isPrime 9
+  let b = isPrime 99
+  let c = isPrime 97
+  let test = a == False && b == False && c == True
+  let result = if test == True 
+               then "Succeeded" 
+               else "Failed"
+  putStrLn (result ++ " Test: testIsPrime")
+
 
 findPrimes :: (Integer, Integer) -> [Integer]
 findPrimes (x, y) = [ z | z <- [x..y], (isPrime z) ]
+
+test :: IO ()
+test = do
+  testIsPrime
 
