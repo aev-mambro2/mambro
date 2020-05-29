f7535c98eb1c5f6c83476bccec22420817267aa3
aev-mambro2 <42442483+aev-mambro2@users.noreply.github.com>
2020-05-29 14:56:09 -0400

    Exercises 7.3, 7.4, and 7.5: pattern matching and cases vs. guards.

--- lib/learning-haskell/One/src/lists.hs
+++ lib/learning-haskell/One/src/lists.hs
@@ -1,3 +1,13 @@
+module Lists (
+    digitsOnly,
+    allCharsAreDigits,
+    makeAllCaps,
+    lettersOnly,
+    makeAllCapsLettersOnly,
+    isPrime,
+    findPrimes,
+    test
+  ) where
 {-
  - Excercises concerning lists and operations on lists
  - @author A.E.Veltstra
@@ -7,15 +17,80 @@
 
 import Prelude
 import Data.Char
+import qualified Control.Exception as Ex
 
+-- |Evaluates the passed-in contents and returns only those
+-- characters that are digits.
 digitsOnly :: String -> String
 digitsOnly x = [ it | it <- x, isDigit it ]
 
+-- |Tests digitsOnly and reports the outcome.
+testDigitsOnly :: IO ()
+testDigitsOnly = do
+  let positiveSubject = "abc123doremi"
+  let positiveOutput = digitsOnly positiveSubject
+  let positiveTest = "123" == positiveOutput
+  let negativeSubject = "abcdef ghj-()@"
+  let negativeOutput = digitsOnly negativeSubject
+  let negativeTest = "" == negativeOutput
+  let test = positiveTest == True && negativeTest == True
+  let result = if test == True
+               then "Succeeded"
+               else "Failed"
+  putStrLn (result ++ " Test: testDigitsOnly")
+
+-- |Evaluates whether all characters in the input are digits.
 allCharsAreDigits :: String -> Bool
 allCharsAreDigits xs = (xs == [x | x <- xs, isDigit x])
 
+-- |Tests allCharsAreDigits and reports the outcome.
+testAllCharsAreDigits :: IO ()
+testAllCharsAreDigits = do
+  let positiveSubject = "94724791"
+  let positiveOutcome = allCharsAreDigits positiveSubject
+  let positiveTest = True == positiveOutcome
+  let negativeSubject = "af6af9fiafna9f8a7df6"
+  let negativeOutcome = allCharsAreDigits negativeSubject
+  let negativeTest = False == negativeOutcome
+  let test = positiveTest == True && negativeTest == True
+  let result = if test == True
+               then "Succeeded"
+               else "Failed"
+  putStrLn (result ++ " Test: testAllCharsAreDigits")
+
+-- |Doubles each of the input numbers
 doubleEach :: [Integer] -> [Integer]
 doubleEach xs = [ 2*x | x <- xs ]
+
+-- |Tests doubleEach positively and reports the outcome
+testDoubleEach :: IO ()
+testDoubleEach = do
+  let input = 8:9:13:78:[]
+  let doubled = doubleEach input
+  let expected = 16:18:26:156:[]
+  let test = expected == doubled
+  let result = if test == True
+               then "Succeeded"
+               else "Failed"
+  putStrLn (result ++ " Test: testDoubleEach")
+
+-- |Tests doubleEach negatively and reports the outcome.
+-- This tests causes and catches an error.
+{-
+testDoubleEachBad :: IO ()
+testDoubleEachBad = do
+  let badInput = "badInput"
+  Ex.catch (show (doubleEach badInput))
+           (\err -> do
+                      let msg = show err
+                      let expected = ""
+                      let test = expected == msg
+                      let result = if test == True
+                                   then "Succeeded"
+                                   else "Failed"
+                      putStrLn (result ++ " Test: testDoubleEachBad 2")
+           )
+-}  
 
 makeAllCaps :: String -> String
 makeAllCaps x = [ toUpper y | y <- x ]
@@ -61,7 +136,63 @@ testIsPrime = do
 findPrimes :: (Integer, Integer) -> [Integer]
 findPrimes (x, y) = [ z | z <- [x..y], (isPrime z) ]
 
+-- |Return the first number in a list + 1. If none found, return 0.
+doExercise71 :: [Integer] -> Integer
+doExercise71 [] = 0
+doExercise71 (x:_) = x + 1
+                       
+-- |Tests exercise 7.1.
+testExercise71 :: IO ()
+testExercise71 = do
+  let input1 = 9:3:8:[]
+  let output1 = doExercise71 input1
+  let test1 = 10 == output1
+  let input2 = []
+  let output2 = doExercise71 input2
+  let test2 = 0 == output2
+  let result = if test1 == True && test2 == True
+               then "Succeeded" 
+               else "Failed"
+  putStrLn (result ++ " Test: testExercise71")
+  
+
+-- |Add the 1st 2 integers in a list, or return head element, or 0
+doExercise72 :: [Integer] -> Integer
+doExercise72 [] = 0
+doExercise72 [x] = x
+{-
+doExercise72 them
+  | 1 == (length them) = them!!0
+  | 1 <= (length them) = them!!0 + them!!1
+-}
+--doExercise72 (x:xs) = x + (xs!!0)
+doExercise72 (x:y:_) = x + y
+
+-- |Tests exercise 7.2.
+testExercise72 :: IO ()
+testExercise72 = do
+  let input1 = [5,3,7]
+  let input2 = [4]
+  let input3 = []
+  let output1 = doExercise72 input1
+  let output2 = doExercise72 input2
+  let output3 = doExercise72 input3
+  let test1 = 8 == output1
+  let test2 = 4 == output2
+  let test3 = 0 == output3
+  let result = if test1 == True && test2 == True && test3 == True
+               then "Succeeded" 
+               else "Failed"
+  putStrLn (result ++ " Test: testExercise72")
+
+
 test :: IO ()
 test = do
   testIsPrime
+  testDigitsOnly
+  testAllCharsAreDigits
+  testDoubleEach
+  --testDoubleEachBad
+  testExercise71
+  testExercise72
 
--- lib/learning-haskell/One/src/recursion.hs
+++ lib/learning-haskell/One/src/recursion.hs
@@ -6,80 +6,133 @@ module Recursion (
   highest
 ) where
 
-  {-|
-   - A factorial is an example of a recursive function.
-   - f(x): f(x-1)*x
-   - Haskell is good at resolving recursion without getting 
-   - lost in stack overflows.
-   -}
-  
-  factor :: Integer -> Integer
-  factor x
-    | x == 0    = 1
-    | x > 0     = factor (x - 1) * x
-    | otherwise = 0
-  
-  
-  repeatSubtractToZero :: Integer -> Integer -> Integer
-  repeatSubtractToZero x y
-    | x > y   = 1 + repeatSubtractToZero (x - y) y
-    | x == y  = 1
-    | otherwise = 0 
-  
-  repeatSubtractToOverflow :: Integer -> Integer -> Integer
-  repeatSubtractToOverflow x y 
-    | x <= 0      = 0
-    | y == 0      = x
-    | y < 0       = error "2nd argument must exceed zero"
-    | (x - y) < 0 = x
-    | otherwise   = repeatSubtractToOverflow (x - y) y
-  
-  divideInt :: Integer -> Integer -> Integer
-  divideInt x y 
-    | y == 0  = error "cannot divide by 0"
-    | x == 0  = 0
-    | y == 1  = x
-    | y == x  = 1
-    | x < 0 && y < 0  = divideInt (-x) (-y)
-    | x < 0   = 0 - divideInt (-x) y
-    | y > x   = 0
-    | y < 0   = 0 - divideInt x (-y)
-    | otherwise = 1 + repeatSubtractToZero (x - y) y
-  
-  half :: Integer -> Integer
-  half x = divideInt x 2
-  
-  highest :: Integer -> Integer -> Integer
-  highest x y 
-    | x == y  = x
-    | x > y   = x
-    | otherwise = y
+{-|
+- A factorial is an example of a recursive function.
+- f(x): f(x-1)*x
+- Haskell is good at resolving recursion without getting 
+- lost in stack overflows.
+-}
+factor :: Integer -> Integer
+factor x
+  | x == 0    = 1
+  | x > 0     = factor (x - 1) * x
+  | otherwise = 0
 
-  lowest :: Integer -> Integer -> Integer
-  lowest x y
-    | x == y  = x
-    | x < y   = x
-    | otherwise = y
-  
-  findRemainderInt :: Integer -> Integer -> Integer
-  findRemainderInt x y
-    | y == 0  = error "cannot divide by 0"
-    | x == 0  = 0
-    | x == y  = 0
-    | y > x   = 0
-    | otherwise = repeatSubtractToOverflow (x - y) y
-  
-  repeatFactorDownToZero :: Integer -> Integer -> Integer -> Integer
-  repeatFactorDownToZero x y z
-    | z == 0 = 0
-    | (findRemainderInt x z) == 0 && (findRemainderInt y z) == 0 = z
-    | otherwise = repeatFactorDownToZero x y (z - 1)
-  
-  -- |Find Highest Common Integer Factor
-  fhcif :: Integer -> Integer -> Integer
-  fhcif x y
-   | x == 0  = 0
-   | y == 0  = 0
-   | x == y  = x
-   | otherwise = repeatFactorDownToZero x y (lowest x y)
-  
+
+repeatSubtractToZero :: Integer -> Integer -> Integer
+repeatSubtractToZero x y
+  | x > y   = 1 + repeatSubtractToZero (x - y) y
+  | x == y  = 1
+  | otherwise = 0 
+
+repeatSubtractToOverflow :: Integer -> Integer -> Integer
+repeatSubtractToOverflow x y 
+  | x <= 0      = 0
+  | y == 0      = x
+  | y < 0       = error "2nd argument must exceed zero"
+  | (x - y) < 0 = x
+  | otherwise   = repeatSubtractToOverflow (x - y) y
+
+divideInt :: Integer -> Integer -> Integer
+divideInt x y 
+  | y == 0  = error "cannot divide by 0"
+  | x == 0  = 0
+  | y == 1  = x
+  | y == x  = 1
+  | x < 0 && y < 0  = divideInt (-x) (-y)
+  | x < 0   = 0 - divideInt (-x) y
+  | y > x   = 0
+  | y < 0   = 0 - divideInt x (-y)
+  | otherwise = 1 + repeatSubtractToZero (x - y) y
+
+half :: Integer -> Integer
+half x = divideInt x 2
+
+highest :: Integer -> Integer -> Integer
+highest x y 
+  | x == y  = x
+  | x > y   = x
+  | otherwise = y
+
+lowest :: Integer -> Integer -> Integer
+lowest x y
+  | x == y  = x
+  | x < y   = x
+  | otherwise = y
+
+findRemainderInt :: Integer -> Integer -> Integer
+findRemainderInt x y
+  | y == 0  = error "cannot divide by 0"
+  | x == 0  = 0
+  | x == y  = 0
+  | y > x   = 0
+  | otherwise = repeatSubtractToOverflow (x - y) y
+
+repeatFactorDownToZero :: Integer -> Integer -> Integer -> Integer
+repeatFactorDownToZero x y z
+  | z == 0 = 0
+  | (findRemainderInt x z) == 0 && (findRemainderInt y z) == 0 = z
+  | otherwise = repeatFactorDownToZero x y (z - 1)
+
+-- |Find Highest Common Integer Factor
+fhcif :: Integer -> Integer -> Integer
+fhcif x y
+  | x == 0  = 0
+  | y == 0  = 0
+  | x == y  = x
+  | otherwise = repeatFactorDownToZero x y (lowest x y)
+
+-- |Exercise 7.4
+product74 :: [Integer] -> Integer
+product74 [] = 1
+product74 [x] = x
+product74 [x,y] = x * y
+product74 (x:xs) = x * (product74 xs)  
+
+testExercise74 :: IO ()
+testExercise74 = do
+  let input1 = []
+  let input2 = [2,3,4]
+  let output1 = product74 input1
+  let output2 = product74 input2
+  let test1 = 1 == output1
+  let test2 = 24 == output2
+  let result = test1 == True && test2 == True
+  let verb = if result == True
+         then "Succeeded"
+         else "Failed"
+  putStrLn (verb ++ " test: testExercise74")
+
+-- |Exercise 7.5
+and75 :: [Bool] -> Bool
+and75 [] = True
+and75 (x:xs) = x && (and75 xs) 
+
+or75 :: [Bool] -> Bool
+or75 [] = False
+or75 (x:xs) = x || (or75 xs)
+
+testExercise75 :: IO ()
+testExercise75 = do
+  let x = [True, True, True]
+  let y = [True, False, False]
+  let z = [False, False, False]
+  let a = and75 x
+  let b = and75 y
+  let c = and75 z
+  let d = or75 x
+  let e = or75 y
+  let f = or75 z
+  let t = a == True && b == False && c == False && d == True && e == True && f == False
+  let v = if t == True
+            then "Succeeded"
+            else "Failed"
+  putStrLn (v ++ " test: testExercise75") 
+
+
+test :: IO ()
+test = do
+  testExercise74
+  testExercise75
+
+
