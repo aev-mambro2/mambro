47810dcce17901e20c3cbbd7aa4772d9f7d232b5
aev-mambro2 <42442483+aev-mambro2@users.noreply.github.com>
2020-07-20 21:42:35 -0400

    Undid haskell formatter foul-ups.

--- lib/learning-haskell/SplitLines/Main.hs
+++ lib/learning-haskell/SplitLines/Main.hs
@@ -6,28 +6,22 @@ import System.Environment
  Copyright  : (c) A.E.Veltstra, 2020
  Maintainer : A.E.Veltstra <aev@sdf.org>
  @since      : 2020-04-29T13:57:00EST
- @version    : 2020-05-13T10:30:00EDT
+ @version    : 2020-07-20T21:41:00EDT
 -}
 
 -- |Writes each contents into its own file. If the file 
 -- exists, it gets overwritten.
-outputFile :: String -> String -> Int -> IO ()
--- ^ the data to write out.
-
--- ^ the pattern for a file path.
-
--- ^ the sequence number to put into the output file name.
-
--- ^ returns the action to execute.
+outputFile :: String -- ^ the data to write out.
+              -> String -- ^ the pattern for a file path.
+              -> Int -- ^ the sequence number to put into the output file name.
+              -> IO () -- ^ returns the action to execute.
 outputFile contents namePattern seq
   = writeFile (namePattern ++ "-" ++ (show seq) ++ ".txt") contents
 
 -- |Writes each of the passed-in records to a file that is named 
 -- after the output file pattern.
-writeFiles :: ([String], String) -> IO ()
--- ^ records, output file pattern. Records should be a list of items, each of which is a record that needs to get saved into its own file. Output file pattern: a pattern for naming a new output file. Will be suffixed with -#.txt in which # equals a sequence number.
-
--- ^ returns the action to execute.
+writeFiles :: ([String], String) -- ^ records, output file pattern. Records should be a list of items, each of which is a record that needs to get saved into its own file. Output file pattern: a pattern for naming a new output file. Will be suffixed with -#.txt in which # equals a sequence number.
+  -> IO () -- ^ returns the action to execute.
 writeFiles (t, outPattern)
   | length t == 0 = (putStrLn "Done")
   | length t == 1 = outputFile (head t) outPattern 1
